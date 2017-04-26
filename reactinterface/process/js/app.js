8a54246986a68a300fc80b6e832947e683077f3a
Daniel <barendan@gmail.com>
2017-04-26 15:08:43 -0400

    Added functionality for updating search dynamically; restructured filteredApts variable

--- reactinterface/process/js/app.js
+++ reactinterface/process/js/app.js
@@ -12,6 +12,7 @@ var MainInterface = React.createClass({
       aptBodyVisible: false,
       orderBy: 'petName',
       orderDir: 'desc',
+      queryText: ' ',
       myAppointments: []
     } //return
   }, //getInitialState
@@ -59,10 +60,30 @@ var MainInterface = React.createClass({
     }); //setState
   }, //reOrder
 
+  searchApts(q) {
+    this.setState({
+      queryText: q
+    }); //setState
+  }, //searchApts
+
   render: function() {
-    var filteredApts = this.state.myAppointments;
+    var filteredApts = [];
     var orderBy = this.state.orderBy;
     var orderDir = this.state.orderDir;
+    var queryText = this.state.queryText;
+    var myAppointments = this.state.myAppointments;
+
+    myAppointments.forEach(function(item) {
+      if(
+        (item.petName.toLowerCase().indexOf(queryText)!=-1) ||
+        (item.ownerName.toLowerCase().indexOf(queryText)!=-1) ||
+        (item.aptDate.toLowerCase().indexOf(queryText)!=-1) ||
+        (item.aptNotes.toLowerCase().indexOf(queryText)!=-1) ||
+      ) {
+        filteredApts.push(item);
+      }
+    }); //forEach
+
 
     filteredApts = _.orderBy(filteredApts, function(item) {
       return item[orderBy].toLowerCase();
@@ -86,7 +107,8 @@ var MainInterface = React.createClass({
         <SearchAppointments 
           orderBy = {this.state.orderBy} 
           orderDir = { this.state.orderDir} 
-          onReOrder = {this.reOrder} />
+          onReOrder = {this.reOrder} 
+          onSearch = {this.searchApts} />
         <ul className="item-list media-list">{filteredApts}</ul>
       </div>
     ) //return
